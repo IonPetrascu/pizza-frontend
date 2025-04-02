e590f7d89abfb501faee2775d8408c7542af55e3
Ion Petrascu <ionpetrascuc0318@gmail.com>
2025-04-02 19:59:10 +0300

    fix(middleware.ts)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,26 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+// import createMiddleware from 'next-intl/middleware';
+// import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+// export default createMiddleware(routing);
+
+// export const config = {
+//     // Match only internationalized pathnames
+//     matcher: ['/', '/(ro|en)/:path*']
+// };
+
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+import { NextRequest } from "next/server";
+
+console.log("Middleware загружен"); // Добавляем лог
+
+const middleware = createMiddleware(routing);
+
+export default function handler(req: NextRequest) {
+  console.log("Middleware вызван для:", req.nextUrl.pathname);
+  return middleware(req);
+}
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(ro|en)/:path*']
+  matcher: ["/", "/(ro|en)/:path*"],
 };
