353f4049bd0bd0a7b84637f8424145706919917e
Ion Petrascu <ionpetrascuc0318@gmail.com>
2025-04-03 11:48:38 +0300

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
-// import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-// // This page only renders when the app is built statically (output: 'export')
-// export default function RootPage() {
-//   redirect('/ro');
-// }
-
-import { redirect } from "next/navigation";
-
+// This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
-  // Проверяем, находимся ли мы на корневом маршруте (корень сайта `/`)
-
-  if (typeof window !== "undefined" && window.location.pathname === "/") {
-    redirect("/en"); // Выполняем редирект только с корня
-  } else {
-    console.log('aaaa');
-  }
-
-  // Альтернативно можно ничего не возвращать для предотвращения дальнейшего рендера
-  return null;
+  redirect('/en');
 }
